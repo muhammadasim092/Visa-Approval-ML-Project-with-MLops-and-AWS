a63a78cbba5b3db35cfaf76e7303842da9ea260d
Muhammad Asim <97046562+muhammadasim092@users.noreply.github.com>
2025-09-14 12:34:26 +0500

    fe

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="visa_approval_prediction",
+    version="0.0.1",
+    author="Muhammad Asim",
+    author_email="asimmughal69@icloud.com",
+    packages=find_packages()
+)
