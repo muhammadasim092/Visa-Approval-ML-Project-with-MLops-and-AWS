c142700935cf954cb9acee605a51f3be9ee650a7
Muhammad Asim <97046562+muhammadasim092@users.noreply.github.com>
2025-10-01 17:33:22 +0500

    updated error remove

--- visa_approval_prediction/constants/__init__.py
+++ visa_approval_prediction/constants/__init__.py
@@ -77,8 +77,8 @@ MODEL_BUCKET_NAME = "visa-prediction-project"
 MODEL_PUSHER_S3_KEY = "model-registry"
 
 
-# APP_HOST = "127.0.0.1"
-APP_HOST = "0.0.0.0"
+APP_HOST = "127.0.0.1"
+# APP_HOST = "0.0.0.0"
 APP_PORT = 8080
 
 
